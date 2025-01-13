850724bd1d63a40c7413b85644e633a03a3aec35
cleopas <namaswacleophas27@gmail.com>
2025-01-13 21:42:11 +0300

    13/1/2025

--- project.java
+++ project.java
@@ -1,83 +1,128 @@
+
 public class project {
+
+    int x = 64;
+
+
+
     public static void main(String[] args) {
-        // String name = "cleopas";
-        // System.out.println("hello world");
-        // System.out.println("the number of characters in the word is  " + name.length());
-        // System.out.println(name.toUpperCase());
-
         
 
-        String[] cars = { "an", "bn", "cn", "dn", "en" };
+         //project myObj2 = new project();
+        //setting the values
+        //myObj2.age(45);
 
-        // for (String i : cars) {
-        //     System.out.println(i);
-        // }
-        // for (int i = 0; i < cars.length; i++) {
-        //     System.out.println(cars[i]);
-        //     System.out.println(cars.length);
+        // staticMethod();
+        // project yourObject = new project();
+        // yourObject.publicMethod();
+        // project myObject = new project();
+        // myObject.x = 78;
+        // System.out.println(myObject.x);
+        // int sum = add(10);
+        // System.out.println("the sum of first 10 numbers is " + sum);
 
-        // }
-        int[] age = { 56, 74, 94, 38 };
-        int[] randomNumber = {45,34,343,343,3434,343,343,343};
+        // int product = multi(42, 72);
 
-        int sumOfAges = addition(age);
-        int sumOfRandomNumbers = addition(randomNumber);
+        // System.out.println("the product of the twonumbers is " + product);
+        // int[] age = { 56, 74, 94, 38 };
+        // int[] randomNumber = { 45, 34, 343, 343, 3434, 343, 343, 343 };
 
-        System.out.println(" the sum of the ages "+ sumOfAges );
-        System.out.println("the sum of the random number "+sumOfRandomNumbers);
+        // int sumOfAges = addition(age);
+        // int sumOfRandomNumbers = addition(randomNumber);
+
+        // System.out.println(" the sum of the ages " + sumOfAges);
+        // System.out.println("the sum of the random number " + sumOfRandomNumbers);
+
+        // // calling a method with return value
+        // int returnAverageValue = returnAverage(age);
+        // System.out.println("printing the return average method " + returnAverageValue);
+
+        // // calling a void method
+        // printAverage(randomNumber);
+
+        // // int[][] marks = { { 45, 89, 62, 27, 27 }, { 45, 74, 37, 38 } };
+
+        // // for (int i = 0; i < marks.length; i++)
+        // // for (int j = 0; j < marks.length; j++) {
+        // // System.out.println(marks[i][j]);
+        // // }
+        // // int rows = marks.length;
+        // // int column = marks[0].length;
+        // // for (int i = 0; i < rows; i++)
+        // // for (int j = 0; j < column; j++) {
+        // // System.out.println(marks[i][j]);
+
+        // // }
+
+        Engine volvoEngine = new Engine();
+        volvoEngine.setCapacity(2);
+        volvoEngine.setFuel("Petrol");
+
+        Car volvo = new Car("volvo");
+        volvo.setColor("green");
+        volvo.setEngine(volvoEngine);
+
+        System.out.println("the color of our volvo is "+ volvo.getColor());
+
+        volvo.drive();;
+
+        Wheels pireli=new Wheels();
+        pireli.setSize(18);
+        System.out.println("the size of wheel is "+ pireli.getSize());
 
 
-        //calling a method with return value
-        int returnAverageValue = returnAverage(age);
-        System.out.println("printing the return average method "+returnAverageValue);
-
-
-        //calling a void method
-        printAverage(randomNumber);
-
-        // int[][] marks = { { 45, 89, 62, 27, 27 }, { 45, 74, 37, 38 } };
-
-        // for (int i = 0; i < marks.length; i++)
-        //     for (int j = 0; j < marks.length; j++) {
-        //         System.out.println(marks[i][j]);
-        //     }
-        // int rows = marks.length;
-        // int column = marks[0].length;
-        // for (int i = 0; i < rows; i++)
-        //     for (int j = 0; j < column; j++) {
-        //         System.out.println(marks[i][j]);
-
-        //     }
-        
-
+        int[] marks={76,78,90};
     }
 
-    //method to add 2 integers
+    // method to add 2 integers
     static int addition(int[] values) {
         int sum = 0;
-        for(int i =0; i < values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             sum = sum + values[i];
         }
         return sum;
     }
 
-    //method to return average of values in an int array
-    static int returnAverage(int[] values){
+    // method to return average of values in an int array
+    static int returnAverage(int[] values) {
         int sum = addition(values);
         int average = sum / values.length;
         return average;
     }
 
-     //method to print average of values in an int array
-    static void printAverage(int[] values){
+    // method to print average of values in an int array
+    static void printAverage(int[] values) {
         int sum = 0;
-        for(int i =0; i < values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             sum = sum + values[i];
         }
         int average = sum / values.length;
-         System.out.println("printing average from void method "+average);
+        System.out.println("printing average from void method " + average);
     }
 
-    //metho sto compare 2 ages
+    public static int multi(int a, int b) {
+        return a * b;
+    }
+
+    public static int add(int k) {
+        if (k > 0) {
+            return k + add(k - 1);
+        } else {
+            return 0;
+        }
+    }
+
+    // creating a static method
+    static void staticMethod() {
+        System.out.println("Here you can access this method without creating an object of the class");
+    }
+
+    // writting a public method
+    public void publicMethod() {
+        System.out.println("Here you need to create an object of the class before calling the method");
+
+    }
 
 }
+
+// metho sto compare 2 ages
